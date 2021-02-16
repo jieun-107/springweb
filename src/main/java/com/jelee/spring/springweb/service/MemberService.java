22f2186b0ed2c46653fe282fc32af8f445f83a5e
jieun <lje7128@gmail.com>
2021-02-17 00:33:06 +0900

    회원 서비스 개발

---
+++ src/main/java/com/jelee/spring/springweb/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.jelee.spring.springweb.service;
+
+import com.jelee.spring.springweb.domain.Member;
+import com.jelee.spring.springweb.repository.MemberRepository;
+import com.jelee.spring.springweb.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     *  회원 가입
+     */
+    public Long join(Member member){
+        // 같은 이름을 가진 중복 회원x
+        validateDuplicateMember(member); // 중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> { // 값이 있으면
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById((memberId));
+    }
+}
