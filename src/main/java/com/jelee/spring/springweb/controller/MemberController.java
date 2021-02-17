76069a65ba8c137a1b923c5dca4f027ba917df68
jieun <lje7128@gmail.com>
2021-02-18 01:10:17 +0900

    회원 조회 기능 추가

--- src/main/java/com/jelee/spring/springweb/controller/MemberController.java
+++ src/main/java/com/jelee/spring/springweb/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.jelee.spring.springweb.domain.Member;
 import com.jelee.spring.springweb.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,13 @@ public class MemberController {
 
         return "redirect:/";
 
+    }
+
+    @GetMapping("members")
+    public String list(Model model) {
+        List<Member> list = memberService.findMembers();
+        model.addAttribute("members", list);
+
+        return "members/memberList";
     }
 }
