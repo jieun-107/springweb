ccf2e902733a85bbdcce5ca37b6b9c939bfe23f0
jieun <lje7128@gmail.com>
2021-02-21 16:30:13 +0900

    스프링 데이터 JPA 코드 추가

--- src/main/java/com/jelee/spring/springweb/SpringConfig.java
+++ src/main/java/com/jelee/spring/springweb/SpringConfig.java
@@ -1,34 +1,32 @@
 package com.jelee.spring.springweb;
 
-import com.jelee.spring.springweb.repository.JpaMemberRepository;
 import com.jelee.spring.springweb.repository.MemberRepository;
 import com.jelee.spring.springweb.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+    /*@Bean
     public MemberRepository memberRepository(){
         // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSourc e);
         // return new JdbcMemberRepository(dataSource);
-        // return new JdbcMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        // return new JpaMemberRepository(em);
+
+    }*/
 }
---
+++ src/main/java/com/jelee/spring/springweb/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,14 @@
+package com.jelee.spring.springweb.repository;
+
+import com.jelee.spring.springweb.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    // JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
+
+}
